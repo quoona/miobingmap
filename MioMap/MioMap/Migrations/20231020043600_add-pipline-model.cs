6c09f091c995e8c78d360e792a57f73546fffaf5
Ly Vi Cuong <chanhbeck27@gmail.com>
2023-10-20 11:54:19 +0700

    add search map

---
+++ MioMap/MioMap/Migrations/20231020043600_add-pipline-model.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MioMap.Migrations
+{
+    /// <inheritdoc />
+    public partial class addpiplinemodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WaterPiplines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Lat1 = table.Column<double>(type: "double", nullable: false),
+                    Long1 = table.Column<double>(type: "double", nullable: false),
+                    Lat2 = table.Column<double>(type: "double", nullable: false),
+                    Long2 = table.Column<double>(type: "double", nullable: false),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    InfoBoxTitle = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    InfoBoxDescription = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Tags = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WaterPiplines", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WaterPiplines");
+        }
+    }
+}
---
+++ MioMap/MioMap/Models/WaterPipline.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MioMap.Models
+{
+    public class WaterPipline
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Lat1 { get; set; }
+        public double Long1 { get; set; }
+        public double Lat2 { get; set; }
+        public double Long2 { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //Infobox
+        public string InfoBoxTitle { get; set; }
+        public string InfoBoxDescription { get; set; }
+        // search tags
+        public string Tags { get; set; }
+    }
+}
